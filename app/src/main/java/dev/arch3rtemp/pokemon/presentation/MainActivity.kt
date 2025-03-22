7313dc76dac12854de2ed057c75a35f029ccb76d
Archil Asanishvili <arch3r_temp@yahoo.com>
2025-03-22 19:48:26 +0400

    * Update libraries up to Android API level 35 * Add .idea directory to .gitignore

--- app/src/main/java/dev/arch3rtemp/pokemon/presentation/MainActivity.kt
+++ app/src/main/java/dev/arch3rtemp/pokemon/presentation/MainActivity.kt
@@ -1,7 +1,11 @@
 package dev.arch3rtemp.pokemon.presentation
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentContainerView
 import dagger.hilt.android.AndroidEntryPoint
 import dev.arch3rtemp.pokemon.R
@@ -12,9 +16,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var container: FragmentContainerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         container = findViewById(R.id.fragmentContainer)
+
+        ViewCompat.setOnApplyWindowInsetsListener(container) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
+
         val homeFragment = HomeFragment.newInstance()
 
         supportFragmentManager
